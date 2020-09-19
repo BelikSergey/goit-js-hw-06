fa6ca3ce8d5e88188d9b3fc1e2b1bd2d7bc47a12
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-09-19 22:53:38 +0300

    Добавляет решение задач с 1 по 5

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,1 +1,15 @@
 "use strict";
+// Write code under this line
+const getUserNames = (users) => users.map(({ name }) => name);
+
+console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
--- JS/tests.js
+++ JS/tests.js
@@ -1,28 +1,40 @@
-const Hero = function (name, xp) {
-  this.name = name;
-  this.xp = xp;
-};
+// const Hero = function (name, xp) {
+//   this.name = name;
+//   this.xp = xp;
+// };
 
-// console.dir(Hero);
+// // console.dir(Hero);
 
-Hero.prototype.gainXp = function (amount) {
-  console.log(`${this.name} gained ${amount} experience points`);
-  this.xp += amount;
-};
+// Hero.prototype.gainXp = function (amount) {
+//   console.log(`${this.name} gained ${amount} experience points`);
+//   this.xp += amount;
+// };
 
-const Warrior = function (name, xp, weapon) {
-  Hero.call(this, name, xp);
+// const Warrior = function (name, xp, weapon) {
+//   Hero.call(this, name, xp);
 
-  this.weapon = weapon;
-};
+//   this.weapon = weapon;
+// };
 
-Warrior.prototype = Object.create(Hero.prototype);
-Warrior.prototype.constructor = Warrior;
-console.log(Warrior.prototype.constructor);
+// Warrior.prototype = Object.create(Hero.prototype);
+// Warrior.prototype.constructor = Warrior;
+// console.log(Warrior.prototype.constructor);
 
-Warrior.prototype.attack = function () {
-  console.log(`${this.name} attacks with ${this.weapon}`);
-};
+// Warrior.prototype.attack = function () {
+//   console.log(`${this.name} attacks with ${this.weapon}`);
+// };
 
-const poly = new Warrior("Poly", 200, "sword");
-// console.dir(poly.attack());
+// const poly = new Warrior("Poly", 200, "sword");
+// // console.dir(poly.attack());
+const users = [
+  { id: "000", name: "Mango", isActive: true },
+  { id: "001", name: "Poly", isActive: false },
+  { id: "002", name: "Ajax", isActive: true },
+  { id: "003", name: "Chelsey", isActive: false },
+];
+
+// Для каждого элемента коллекции (user) проверим поле id.
+// Если оно совпадает с искомым идентификатором, то find прекратит
+// выполнение и вернет текущий элемент (user) как результат выполнения
+const user = users.find((user) => user.id === "002");
+console.log(user);
